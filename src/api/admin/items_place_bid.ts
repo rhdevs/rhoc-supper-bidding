20a848bbfe6b3da2ef7f9af624f4c90c7ad794b7
farhan-navas <farhanmnavas@gmail.com>
2024-08-04 01:20:18 +0800

    resolve deployment error

--- src/api/admin/items_place_bid.ts
+++ src/api/admin/items_place_bid.ts
@@ -1,9 +1,9 @@
-import { DocumentData, QueryDocumentSnapshot } from "@firebase/firestore";
-import { db } from "@/firebase.ts";
+// import { DocumentData, QueryDocumentSnapshot } from "@firebase/firestore";
+// import { db } from "@/firebase.ts";
 
-const placeBid = async (groupRef: QueryDocumentSnapshot<DocumentData>, itemRef: QueryDocumentSnapshot<DocumentData>, pointsBid: number) => {
-  if (groupRef.data().points < pointsBid) {
-    throw new Error("Not enough points");
-  }
+// const placeBid = async (groupRef: QueryDocumentSnapshot<DocumentData>, itemRef: QueryDocumentSnapshot<DocumentData>, pointsBid: number) => {
+//   if (groupRef.data().points < pointsBid) {
+//     throw new Error("Not enough points");
+//   }
   
-}
+// }
